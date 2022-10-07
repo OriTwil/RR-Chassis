7ac268e0593ea6cb024cddd79b389b4f5f3003f7
szf <2176529058@qq.com>
2022-10-07 14:59:19 +0800

    usermain头文件及注释

---
+++ usercode/user_inc/usermain.h
@@ -0,0 +1,26 @@
+#ifndef USERMAIN_H
+#define USERMAIN_H
+#include "cmsis_os.h"
+#include "can.h"
+#include "dma.h"
+#include "usart.h"
+#include "gpio.h"
+#include "Caculate.h"
+#include "wtr_can.h"
+#include "DJI.h"
+#include "wtr_uart.h"
+#include <math.h>
+#include "main.h"
+
+#define pi 3.1415926535898
+#define DEC (pi/180)
+#define r_underpan 0.1934
+#define r_wheel 0.076
+
+void calculate(double * moter_speed,
+               double v_x,
+               double v_y,
+               double v_w);
+
+extern double moter_speed [3];
+#endif
--- usercode/user_src/usermian.c
+++ usercode/user_src/usermian.c
@@ -1,23 +1,6 @@
-#include "main.h"
 #include "usermian.h"
-#include "cmsis_os.h"
-#include "can.h"
-#include "dma.h"
-#include "usart.h"
-#include "gpio.h"
-#include "Caculate.h"
-#include "wtr_can.h"
-#include "DJI.h"
-#include "wtr_uart.h"
-#include <math.h>
-#define pi 3.1415926535898
-#define DEC (pi/180)
-#define r_underpan 0.1934
-#define r_wheel 0.076
 
-double moter_speed [3];
-
-
+//将底盘速度解算到电机速度
 void calculate(double * moter_speed,
                double v_x,
                double v_y,
@@ -28,17 +11,19 @@ void calculate(double * moter_speed,
     moter_speed[2] = (- v_x * sin(30 * DEC) + v_y * cos(30 * DEC)  + v_w * r_underpan)/(2 * pi * r_wheel);
 }
 
+//线程一：底盘控制
 void thread_1(void const * argument)
 {
+    //初始化设置
     CANFilterInit(&hcan1);//è¿‡æ»¤å™¨è®¾ï¿???
     hDJI[0].motorType = M3508;
     hDJI[1].motorType = M3508;
     hDJI[2].motorType = M3508;//ç”µæœºç±»åž‹è®¾ç½®
     DJI_Init();
-    
+    //串口接收信息
     HAL_UART_Receive_DMA(&huart1,JoyStickReceiveData,18);
 	osDelay(100);
-    
+    //解算，速度伺服
     for(;;){
     calculate(moter_speed,crl_speed.vx,crl_speed.vy,crl_speed.vw);
     speedServo(moter_speed[0],&hDJI[0]);
@@ -56,7 +41,7 @@ void thread_1(void const * argument)
 
 }
 
-
+//创建线程
 void StartDefaultTask(void const * argument)
 {
     osThreadDef(underpan, thread_1, osPriorityNormal, 0, 512);
@@ -68,6 +53,7 @@ void StartDefaultTask(void const * argument)
         osDelay(1);
     }
 }
+//串口回调函数，解码
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
     UART1Decode();
