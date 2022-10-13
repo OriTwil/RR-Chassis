dd0fec29b8b3ad4c82fdb5f9a7c69d1b977b14c1
szf <2176529058@qq.com>
2022-10-13 23:23:48 +0800

    增加四轮全向移动底盘功能

--- usercode/user_src/usermian.c
+++ usercode/user_src/usermian.c
@@ -19,10 +19,10 @@ void calculate(double * moter_speed,
                double v_y,
                double v_w);
 
-double moter_speed [3];
+double moter_speed [4];
 
 //将底盘速度解算到电机速度
-void calculate(double * moter_speed,
+void calculate_3(double * moter_speed,
                double v_x,
                double v_y,
                double v_w)
@@ -30,7 +30,18 @@ void calculate(double * moter_speed,
     moter_speed[0] = (- v_x * sin(30 * DEC) - v_y * cos(30 * DEC)  + v_w * r_underpan)/(2 * pi * r_wheel);
     moter_speed[1] = (+ v_x                        + v_w * r_underpan)/(2 * pi * r_wheel);
     moter_speed[2] = (- v_x * sin(30 * DEC) + v_y * cos(30 * DEC)  + v_w * r_underpan)/(2 * pi * r_wheel);
-}
+}//三轮全向
+
+void calculate_4(double * moter_speed,
+               double v_x,
+               double v_y,
+               double v_w)
+{
+    moter_speed[0] =  v_y + v_w;
+    moter_speed[1] = -v_x + v_w;
+    moter_speed[0] = -v_y + v_w;
+    moter_speed[0] =  v_x + v_w;
+}//四轮全向
 
 //线程一：底盘控制
 void thread_1(void const * argument)
@@ -40,6 +51,7 @@ void thread_1(void const * argument)
     hDJI[0].motorType = M3508;
     hDJI[1].motorType = M3508;
     hDJI[2].motorType = M3508;//ç”µæœºç±»åž‹è®¾ç½®
+    hDJI[3].motorType = M3508;
     DJI_Init();
     //串口接收信息
 
@@ -48,15 +60,16 @@ void thread_1(void const * argument)
     //解算，速度伺服
     for(;;){
 
-    calculate(moter_speed,crl_speed.vx,crl_speed.vy,crl_speed.vw);
+    calculate_4(moter_speed,crl_speed.vx,crl_speed.vy,crl_speed.vw);
     speedServo(moter_speed[0],&hDJI[0]);
     speedServo(moter_speed[1],&hDJI[1]);
     speedServo(moter_speed[2],&hDJI[2]);
+    speedServo(moter_speed[3],&hDJI[3]);
 
     CanTransmit_DJI_1234(&hcan1,hDJI[0].speedPID.output,
                                 hDJI[1].speedPID.output,
                                 hDJI[2].speedPID.output,
-                                0);
+                                hDJI[3].speedPID.output);
     osDelay(1);
     }
 
